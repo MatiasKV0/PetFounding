c688868e23f2c14a43f8349d491c87ea0eceebf2
MatiasKV0 <matiasvlln@gmail.com>
2025-12-16 23:31:37 -0300

    add slices adopciones and donaciones

--- client/src/api/apiSlice.js
+++ client/src/api/apiSlice.js
@@ -5,7 +5,7 @@ const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: `${BASE_URL}/api/v1` }), 
-  tagTypes: ['Mascotas', 'Adopciones'],
+  tagTypes: ['Mascotas', 'Adopciones', 'Donaciones', 'Users'],
   endpoints: (builder) => ({
     // Endpoints serán inyectados en otros archivos
   }),
