2df6252154aecd2a5accea4eca4c263e34d0c4f6
camilalazaroavila <camilalazaro2005@gmail.com>
2025-12-20 12:14:47 -0300

    rename HomeController to TestController

--- backend/src/main/java/com/petFounding/controller/TestController.java
+++ backend/src/main/java/com/petFounding/controller/TestController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-public class HomeController {
+public class TestController {
 
     @Autowired
     private com.petFounding.interfacee.ShelterService shelterService;
